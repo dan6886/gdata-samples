6604e82d39644e5991a67aa9d01226a4db88331e
api.jhartmann@google.com <api.jhartmann@google.com@14958b2e-7955-0410-8a8d-a5d105d17ecf>
2009-02-21 00:10:47 +0000

    Added username search box and some changes to CSS and backend.

--- tubestalker/js/frontend.js
+++ tubestalker/js/frontend.js
@@ -1,33 +1,86 @@
-// TODO 
-// ADD HEADERS
-// copyright
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ * Author: Jochen Hartmann <api.jhartmann@google.com>
+ */
 
+/**
+ * @fileoverview Frontend to handle events and DOM manipulation for
+ * YouTube activity feed application. Provides functions for fetching YouTube
+ * Data API feeds using a PHP backend.
+ */
+
+/**
+ * Provides namespacing for the YouTube Activity Application (ytActivityApp)
+ */
 var ytActivityApp = {};
 
+/**
+ * The base URI that the frontend communicates with via JSON
+ * @type String
+ */
 ytActivityApp.URI = 'index.php';
+
+/**
+ * The URI that a user would visit to log out
+ * @type String
+ */
 ytActivityApp.LOGOUT_URI = 'index.php?action=logout';
 
-// divs
+/**
+ * The div Id to write activity stream results to
+ * @type String
+ */
 ytActivityApp.FEED_RESULTS_DIV = 'activity_stream';
-ytActivityApp.FEED_RESULTS_FRIEND_DIV = 'friend_activity_stream';
-ytActivityApp.LOGGING_DIV = 'log';
+
+/**
+ * The div Id to write log in and log out activity to
+ * @type String
+ */
 ytActivityApp.USER_LOGIN_DIV = 'loginlogout';
 
-// css
-ytActivityApp.CSS_FEED_LI_CLASSNAME = 'feed_item';
+/**
+ * The CSS classname for API not available messages
+ * @type String
+ */
+ytActivityApp.CSS_API_NOT_AVAILABLE_CLASSNAME = 'api_not_available';
+
+/**
+ * The CSS classname for activity entry divs
+ * @type String
+ */
 ytActivityApp.CSS_ENTRY_DIV_CLASSNAME = 'activity_entry';
-ytActivityApp.CSS_ENTRY_THUMB_DIV_CLASSNAME = 'video_thumbnail';
-// for rating and view count
+
+/**
+ * The CSS classname for activity entry divs
+ * @type String
+ */
 ytActivityApp.CSS_ENTRY_METADATA_SPAN_CLASSNAME = 'video_metadata';
-ytActivityApp.CSS_ENTRY_HIDDEN_VIDEO_DIV_CLASSNAME = 'videobox';
+ytActivityApp.CSS_ENTRY_MY_USERNAME_LINK_CLASSNAME = 'my_username_link';
+ytActivityApp.CSS_ENTRY_THUMB_DIV_CLASSNAME = 'video_thumbnail';
 ytActivityApp.CSS_ENTRY_TIMESTAMP_SPAN_CLASSNAME = 'activity_timestamp';
 ytActivityApp.CSS_ENTRY_USERNAME_LINK_CLASSNAME = 'username_link';
-ytActivityApp.CSS_ENTRY_MY_USERNAME_LINK_CLASSNAME = 'my_username_link';
-ytActivityApp.CSS_ENTRY_VIDEO_TITLE_SPAN_CLASSNAME = 'video_title';
 ytActivityApp.CSS_ENTRY_VIDEO_ID_SPAN_CLASSNAME = 'video_id';
 ytActivityApp.CSS_ENTRY_VIDEO_METADATA_NOT_FOUND_CLASSNAME = 'metadata_not_found';
+ytActivityApp.CSS_ENTRY_VIDEO_TITLE_SPAN_CLASSNAME = 'video_title';
+ytActivityApp.CSS_FEED_LI_CLASSNAME = 'feed_item';
 ytActivityApp.CSS_USER_ACTIVITY_NOT_FOUND_CLASSNAME = 'user_activity_not_found';
-ytActivityApp.CSS_API_NOT_AVAILABLE_CLASSNAME = 'api_not_available';
+
 // metadata
 ytActivityApp.METADATA_TITLE_NOT_FOUND = 'title not found';
 ytActivityApp.METADATA_UPLOADER_NOT_FOUND = 'uploader not found';
@@ -63,11 +116,16 @@ ytActivityApp.FORM_RADIO_SELECTION_ID = 'feed_type_select';
 
 
 $(document).ready(function(){
-  $('#status').html(ytActivityApp.LOADING_IMAGE_HTML);
-  if (ytActivityApp.FEED_REQUESTED == ytActivityApp.USER_ACTIVITY_FEED) {
-    ytActivityApp.getActivityFeed();
-  } else {
-    ytActivityApp.getFriendActivityFeed();
+    $('#status').html(ytActivityApp.LOADING_IMAGE_HTML);
+
+
+  if (loggedIn) {
+    $('#options').slideDown("slow");
+    if (ytActivityApp.FEED_REQUESTED == ytActivityApp.USER_ACTIVITY_FEED) {
+      ytActivityApp.getActivityFeed();
+    } else {
+      ytActivityApp.getFriendActivityFeed();
+    }
   }
 });
 
@@ -91,12 +149,47 @@ ytActivityApp.switchFeedURI = function() {
 // reset to activity
 ytActivityApp.resetFormSelection = function() {
   var form = document.getElementById(ytActivityApp.FORM_RADIO_SELECTION_ID);
-  form[0].checked = true;form[1].checked = false;
+  form[0].checked = true; form[1].checked = false;
   
 }
 
+ytActivityApp.getActivityFeedForManyUsers = function(usernames) {
+  // sanitize input...
+  // YT: Your username can only contain letters A-Z or numbers 0-9 
+  // split on comma or white space or both
+  // extract all whitespace first
+  if ((usernames == null) || (usernames == '')) {
+    $('#status').show().html('Oops! Looks like no usernames were entered ...')
+      .css('color', 'red');
+    return;
+  } 
 
-
+  var usernameArray = usernames.split(',');
+  var numUsernames = usernameArray.length;
+  var validUsernames = [];
+  
+  if (numUsernames > 20) {
+    $('#status').show().html('Hey! You submitted more than 20 usernames, ' + 
+      'please try again. A maximum of 20 usernames is supported.')
+      .css('color', 'red');
+    return;
+  }
+  
+  for (i = 0; i < numUsernames; i++) {
+    // strip whitespace
+    var username = $.trim(usernameArray[i]);
+    var illegalCharacters = /[^a-zA-Z0-9]/;
+    if (illegalCharacters.test(username)) {
+      $('#status').show().html('Ouch! <strong>' + username +
+        '</strong> is not a valid YT username! Please try again.')
+        .css('color', 'red');
+        return;
+    } else {
+      validUsernames.push(username);
+    }
+  }
+  ytActivityApp.getActivityFeed(validUsernames.join(','), null);
+}
 
 ytActivityApp.getActivityFeed = function(username, friendfeed) {
   if (loggedIn == true) {
@@ -278,7 +371,7 @@ ytActivityApp.processJSON = function(data) {
               }
               if (thumbnail_url != ytActivityApp.METADATA_THUMBNAIL_URL_NOT_FOUND) {
                 HTML_string.push('<div id="' + ytActivityApp.CSS_ENTRY_THUMB_DIV_CLASSNAME + '">');
-                //ytActivityApp.CSS_ENTRY_HIDDEN_VIDEO_DIV_CLASSNAME
+
                 if(player_url) {
                   player_url = player_url + '&autoplay=1';
                   added_video = true;
