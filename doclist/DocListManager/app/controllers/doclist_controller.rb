52664a3d73ee599f8a8bad4e3c12e0a86b37f18f
api.eric@google.com <api.eric@google.com@14958b2e-7955-0410-8a8d-a5d105d17ecf>
2009-08-29 04:14:54 +0000

    Updating sample to be Doclist API v3.0 compatible: - Download pdfs - Adding new metadata to doc preview

--- doclist/DocListManager/app/controllers/doclist_controller.rb
+++ doclist/DocListManager/app/controllers/doclist_controller.rb
@@ -15,12 +15,12 @@
 class DoclistController < ApplicationController
   layout 'standard'
   before_filter :setup_client, :set_user_email
-  
+
   def all
     url = params[:url] ? params[:url] + "/-/#{MINE_LABEL}" :
                          DOCLIST_FEED + "/-/#{MINE_LABEL}/#{DOCUMENT_DOC_TYPE}"
     url += '?showfolders=true'
-    
+
     begin
       feed = @client.get(url).to_xml
       @documents = create_docs(feed)
@@ -35,75 +35,75 @@ class DoclistController < ApplicationController
       render :partial => 'documents_list'
     end
   end
-  
+
   def documents
     @doc_type = DOCUMENT_DOC_TYPE
     get_documents_for(:category => [@doc_type, MINE_LABEL])
   end
-  
+
   def spreadsheets
     @doc_type = SPREADSHEET_DOC_TYPE
     get_documents_for(:category => [@doc_type, MINE_LABEL])
   end
-  
+
   def presentations
     @doc_type = PRESO_DOC_TYPE
     get_documents_for(:category => [@doc_type, MINE_LABEL])
   end
-  
+
   def pdfs
     @doc_type = PDF_DOC_TYPE
     get_documents_for(:category => [@doc_type, MINE_LABEL])
   end
-  
+
   def folders
     @doc_type = FOLDER_DOC_TYPE
     get_documents_for(:category => [@doc_type, MINE_LABEL],
                       :params=>'showfolders=true')
   end
-  
+
   def starred
     @doc_type = DOCUMENT_DOC_TYPE
     get_documents_for(:category => [STARRED_LABEL, MINE_LABEL],
                       :params=>'showfolders=true')
   end
-  
+
   def trashed
     @doc_type = DOCUMENT_DOC_TYPE
     get_documents_for(:category => [TRASHED_LABEL, MINE_LABEL],
                       :params=>'showfolders=true')
   end
-  
+
   def show
     # expandAcl projection will inline the ACLs in the resulting feed
     url = params[:url].sub(/\/full/, '/expandAcl')
-    
+
     entry = @client.get(url).to_xml
     @document = create_doc(entry)
     if @document.type == DOCUMENT_DOC_TYPE or @document.type == PRESO_DOC_TYPE
       export_url = @document.links['export'] + '&exportFormat=png'
-      # Src value for an imaging containing a data URI
+      # Src value for an image containing a data URI
       @preview_img = Base64.encode64(download(export_url))
     end
     render :partial => 'show'
   end
-  
+
   def download(export_url=nil)
     export_url ||= params[:export_url]
- 
+
     resp = @client.get(export_url)
 
     # Set our response headers based on those returned with the file.
     headers['content-type'] = resp.headers['content-type']
     headers['content-disposition'] = resp.headers['content-disposition']
-    
+
     if params[:export_url]
       render :text => resp.body
     else
       return resp.body
     end
   end
-  
+
   def set_acls
     return unless request.xhr?
 
@@ -135,7 +135,7 @@ class DoclistController < ApplicationController
       end
     end
   end
-  
+
   def logout
     @client.auth_handler.revoke
     session[:users_email] = nil
@@ -143,7 +143,7 @@ class DoclistController < ApplicationController
 
     redirect_to '/'
   end
-  
+
 private
 
   def set_user_email
@@ -153,24 +153,24 @@ private
       session[:users_email] = feed.elements['author/email'].text
     end
   end
-  
-  def doc_type(entry)
-    entry.each_element_with_attribute('scheme',
-                                      'http://schemas.google.com/g/2005#kind',
-                                      0, 'category') do |category|
-      return category.attributes['label']
-    end
-  end
-  
-  def create_doc(entry)
-    doc = GDoc::Document.new(entry.elements['title'].text,
-                             :type => doc_type(entry),
-                             :xml => entry.to_s)
-    # Extract the document/spreadsheet/presentation id from the <atom:id>
-    doc.doc_id = entry.elements['id'].text[/#{doc.type}%3[aA](.*)\/?$/, 1]
 
+  def create_doc(entry)
+    resource_id = entry.elements['gd:resourceId'].text.split(':')
+    doc = GDoc::Document.new(entry.elements['title'].text,
+                             :type => resource_id[0],
+                             :xml => entry.to_s)
+
+    doc.doc_id = resource_id[1]
     doc.last_updated = DateTime.parse(entry.elements['updated'].text)
-    
+    if !entry.elements['gd:lastViewed'].nil?
+      doc.last_viewed = DateTime.parse(entry.elements['gd:lastViewed'].text)
+    end
+    if !entry.elements['gd:lastModifiedBy/email'].nil?
+       doc.last_modified_by = entry.elements['gd:lastModifiedBy/email'].text
+    end
+    doc.writers_can_invite = entry.elements['docs:writersCanInvite'].attributes['value']
+    doc.author = entry.elements['author/email'].text
+
     entry.elements.each('link') do |link|
       doc.links[link.attributes['rel']] = link.attributes['href']
     end
@@ -184,8 +184,10 @@ private
       when SPREADSHEET_DOC_TYPE
         doc.links['export'] = SPREADSHEETS_SCOPE +
                               "download/spreadsheets/Export?key=#{doc.doc_id}"
+      when PDF_DOC_TYPE
+        doc.links['export'] = doc.links['content_src']
     end
-    
+
     entry.elements.each('gd:feedLink/feed/entry') do |feedlink_entry|
       email = feedlink_entry.elements['gAcl:scope'].attributes['value']
       role = feedlink_entry.elements['gAcl:role'].attributes['value']
@@ -193,7 +195,7 @@ private
     end
     return doc
   end
-  
+
   def create_docs(feed)
     documents = []
     feed.elements.each('entry') do |entry|
@@ -202,7 +204,7 @@ private
     end
     return documents
   end
-  
+
   def get_documents_for(options={})
     options[:category] ||= [MINE_LABEL]
 
@@ -221,5 +223,5 @@ private
       render :partial => 'documents_list'
     end
   end
-  
+
 end
