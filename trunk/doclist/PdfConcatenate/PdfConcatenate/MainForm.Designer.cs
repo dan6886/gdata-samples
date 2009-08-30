343cbf01cf6a8c7dc398f6ea31a4f7749d9f6dd5
api.eric@google.com <api.eric@google.com@14958b2e-7955-0410-8a8d-a5d105d17ecf>
2009-08-30 10:46:28 +0000

    PdfConcatenate sample for DocList v3.0 PDF upload

---
+++ trunk/doclist/PdfConcatenate/PdfConcatenate/MainForm.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.usernameBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.filelistView = new System.Windows.Forms.ListView();
+            this.uploadButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.outputPdfTitleBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // usernameBox
+            // 
+            this.usernameBox.Location = new System.Drawing.Point(65, 8);
+            this.usernameBox.Name = "usernameBox";
+            this.usernameBox.Size = new System.Drawing.Size(241, 20);
+            this.usernameBox.TabIndex = 0;
+            this.usernameBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.usernameBox_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "password";
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Location = new System.Drawing.Point(65, 34);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.PasswordChar = '*';
+            this.passwordBox.Size = new System.Drawing.Size(241, 20);
+            this.passwordBox.TabIndex = 2;
+            this.passwordBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.passwordBox_KeyPress);
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(234, 60);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 4;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // filelistView
+            // 
+            this.filelistView.FullRowSelect = true;
+            this.filelistView.GridLines = true;
+            this.filelistView.Location = new System.Drawing.Point(10, 136);
+            this.filelistView.Name = "filelistView";
+            this.filelistView.Size = new System.Drawing.Size(358, 243);
+            this.filelistView.TabIndex = 5;
+            this.filelistView.UseCompatibleStateImageBehavior = false;
+            this.filelistView.View = System.Windows.Forms.View.List;
+            this.filelistView.MouseUp += new System.Windows.Forms.MouseEventHandler(this.filelistView_MouseUp);
+            this.filelistView.MouseDown += new System.Windows.Forms.MouseEventHandler(this.filelistView_MouseDown);
+            this.filelistView.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.filelistView_KeyPress);
+            // 
+            // uploadButton
+            // 
+            this.uploadButton.Enabled = false;
+            this.uploadButton.Location = new System.Drawing.Point(112, 385);
+            this.uploadButton.Name = "uploadButton";
+            this.uploadButton.Size = new System.Drawing.Size(165, 23);
+            this.uploadButton.TabIndex = 6;
+            this.uploadButton.Text = "Concatenate PDFs and Upload";
+            this.uploadButton.UseVisualStyleBackColor = true;
+            this.uploadButton.Click += new System.EventHandler(this.uploadButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApplication1.Properties.Resources.docs_large;
+            this.pictureBox1.Location = new System.Drawing.Point(315, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 62);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // outputPdfTitleBox
+            // 
+            this.outputPdfTitleBox.Location = new System.Drawing.Point(10, 110);
+            this.outputPdfTitleBox.Name = "outputPdfTitleBox";
+            this.outputPdfTitleBox.Size = new System.Drawing.Size(227, 20);
+            this.outputPdfTitleBox.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Title for ouput PDF:";
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(389, 417);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.outputPdfTitleBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.uploadButton);
+            this.Controls.Add(this.filelistView);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.usernameBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Google Docs PDF Concatenator";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainForm_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox usernameBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.ListView filelistView;
+        private System.Windows.Forms.Button uploadButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox outputPdfTitleBox;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
