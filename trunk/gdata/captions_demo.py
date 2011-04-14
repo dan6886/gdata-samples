a6b0ac956a48dbc92b0a87ac95abde2d600d3fb9
api.jeffy <api.jeffy@14958b2e-7955-0410-8a8d-a5d105d17ecf>
2011-04-14 18:58:42 +0000

    Changes captions_demo.py to do a (naive) Pig Latin translation instead of using the Google Translation API.

--- trunk/gdata/captions_demo.py
+++ trunk/gdata/captions_demo.py
@@ -18,8 +18,7 @@
 """Demonstration of the YouTube API's captions features.
 
 This script retrieves the ASR captions track for a video in your YouTube
-account, translates it into a different language using the Google Translation
-API, and then uploads it back to YouTube.
+account, translates it into Pig Latin, and then uploads it back to YouTube.
 """
 
 __author__ = "jeffy@google.com (Jeffrey Posnick)"
@@ -41,15 +40,14 @@ class Error(Exception):
   pass
 
 class CaptionsDemo(object):
-  """A class to interact with the YouTube and Translations APIs."""
+  """A class to interact with the YouTube Captions API."""
 
   # CONSTANTS
   # The client id, secret, and developer key are copied from
   # the Google APIs Console <http://code.google.com/apis/console>
   CLIENT_ID = "209139100509.apps.googleusercontent.com"
   CLIENT_SECRET = "N7mQzEhtdwcaniYfbENiVber"
-  TRANSLATIONS_DEVELOPER_KEY = "AIzaSyAZuxr0GdFNinM1BBgTVaYRJrjyXeXmYnk"
-
+  
   # Register for a YouTube API developer key at
   # <http://code.google.com/apis/youtube/dashboard/>
   YOUTUBE_DEVELOPER_KEY = "AI39si5GEgLYKzZxKfv9cue2iKk82oD7SvAIaCxVkgyT9lqKCgd4DIFnGWKWIa3aFj05IhBT2hZ7pHkxGqJXM48ptOmS8ngsLg"
@@ -58,12 +56,12 @@ class CaptionsDemo(object):
   OAUTH_SCOPE = "https://gdata.youtube.com"
   CAPTIONS_URL_FORMAT = "http://gdata.youtube.com/feeds/api/videos/%s/captions?alt=json"
   CAPTIONS_CONTENT_TYPE = "application/vnd.youtube.timedtext; charset=UTF-8"
-  CAPTIONS_TITLE = "Machine Translation of Machine Transcription"
+  CAPTIONS_TITLE = "Pig Latin"
+  CAPTIONS_LANGUAGE_CODE = "en"
 
-  def __init__(self, video_id, target_language):
+  def __init__(self, video_id):
     """Inits CaptionsDemo with the command line arguments."""
     self.video_id = video_id
-    self.target_language = target_language
 
   def Authenticate(self):
     """Handles OAuth2 authentication.
@@ -100,13 +98,13 @@ class CaptionsDemo(object):
     }
     self.http = self.credentials.authorize(httplib2.Http())
 
-  def GetAsrTrackInfo(self):
-    """Retrieves URL and source language for the ASR track.
+  def GetAsrTrackUrl(self):
+    """Retrieves URL for the ASR track.
 
     The YouTube API has a REST-ful interface for retrieving a list of captions
     tracks for a given video. We request that list in a JSON response, and then
-    loop through the captions tracks until we find the ASR. We save its unique
-    URL and the source language for the track.
+    loop through the captions tracks until we find the ASR. We save the unique
+    URL for the track.
 
     Raises:
       Error: The ASR caption track info could not be retrieved.
@@ -117,17 +115,14 @@ class CaptionsDemo(object):
     if response_headers["status"] == "200":
       json_response = json.loads(body)
       for entry in json_response["feed"]["entry"]:
-        if "yt$derived" in entry and entry["yt$derived"]["$t"] == "speechRecognition":
+        if "yt$derived" in entry and entry["yt$derived"]["$t"] == "speechRecognition" and entry["content"]["xml$lang"] == self.CAPTIONS_LANGUAGE_CODE:
           # This will only be set for the ASR track.
           self.track_url = entry["content"]["src"]
-          self.source_language = entry["content"]["xml$lang"]
-          if self.source_language == self.target_language:
-            raise Error("Source and target language are both %s" % self.source_language)
     else:
       raise Error("Received HTTP response %s when requesting %s." % (response_headers["status"], url))
 
     if self.track_url is None:
-      raise Error("Could not find an ASR captions track for this video.")
+      raise Error("Could not find an appropriate ASR captions track for this video.")
 
   def GetSrtCaptions(self):
     """Retrieves and parses the actual ASR captions track's data.
@@ -145,6 +140,41 @@ class CaptionsDemo(object):
     else:
       raise Error("Received HTTP response %s when requesting %s?fmt=srt." % (response_headers["status"], self.track_url))
 
+  def TranslateWord(self, word):
+    """Translates a given word into its Pig Latin equivalent.
+    
+    Native Pig Latin speakers--please forgive any errors in this algorithm!
+    This is used for purposes of demonstration only.
+    
+    Args:
+      word: A word to translate to Pig Latin.
+    """
+    vowels = "aeiouy"
+    if word.isalpha():
+      if word[0] in vowels:
+        return word + "way"
+      else:
+        return word[1:] + word[0] + "ay"
+    else:
+      return word
+    
+  def TranslateSetence(self, sentence):
+    """Translates a sentence into its Pig Latin equivalent.
+    
+    Naively breaks up the sentence into words, and calls TranslateWord on each.
+    Then attempst to reconstruct the sentence and capitalize everything.
+    
+    Args:
+      sentence: A sentence to translate to Pig Latin.
+    """
+    translated_words = []
+    for word in sentence.split():
+      translated_words.append(self.TranslateWord(word))
+      
+    translated_sentence = " ".join(translated_words)
+    return translated_sentence.capitalize()
+      
+
   def TranslateCaptions(self):
     """Uses the Google Translation API to translate the captions track.
 
@@ -152,27 +182,14 @@ class CaptionsDemo(object):
     Once the translation is done, the caption items are assigned the new
     translations, and the SubRipFile object is serialized into a string.
     """
-    # We want just the text (not the timecodes) for the ASR captions.
-    original_captions = []
-    for srt_caption_item in self.srt_captions:
-      original_captions.append(srt_caption_item.text.strip())
-
-    translation_service = build(
-      "translate",
-      "v2",
-      developerKey=self.TRANSLATIONS_DEVELOPER_KEY)
-
-    translation_response = translation_service.translations().list(
-      source=self.source_language,
-      target=self.target_language,
-      q=original_captions).execute()
-
     self.translated_captions_body = ""
-    # Loop through each of the caption items and assign it the corresponding
-    # translation, encoded in UTF-8. Serialize everything into a string.
-    for i in range(0, len(self.srt_captions)):
-      self.srt_captions[i].text = translation_response["translations"][i]["translatedText"] + "\n"
-      self.translated_captions_body += unicode(self.srt_captions[i]).encode("utf-8")
+    for srt_caption_item in self.srt_captions:
+      # We want just the text (not the timecodes) for the ASR captions.
+      # Each caption item might be more than one sentence, but this is good
+      # for our purposes.
+      sentence = srt_caption_item.text.strip()
+      srt_caption_item.text = self.TranslateSetence(sentence) + "\n"
+      self.translated_captions_body += unicode(srt_caption_item).encode("utf-8")
 
   def UploadTranslatedCaptions(self):
     """Uploads the newly translated captions data via the YouTube API.
@@ -183,14 +200,10 @@ class CaptionsDemo(object):
       Error: The ASR caption track could not be uploaded.
     """
     self.headers["Content-Type"] = self.CAPTIONS_CONTENT_TYPE
-    self.headers["Content-Language"] = self.target_language
+    self.headers["Content-Language"] = self.CAPTIONS_LANGUAGE_CODE
     self.headers["Slug"] = self.CAPTIONS_TITLE
     url = self.CAPTIONS_URL_FORMAT % self.video_id
-    response_headers, body = self.http.request(
-      url,
-      "POST",
-      body=self.translated_captions_body,
-      headers=self.headers)
+    response_headers, body = self.http.request(url, "POST", body=self.translated_captions_body, headers=self.headers)
 
     if response_headers["status"] != "201":
       raise Error("Received HTTP response %s when uploading captions to %s." % (response_headers["status"], url))
@@ -200,12 +213,12 @@ class CaptionsDemo(object):
     try:
       self.Authenticate()
       self.SetupHttpRequestObject()
-      self.GetAsrTrackInfo()
+      self.GetAsrTrackUrl()
       self.GetSrtCaptions()
       self.TranslateCaptions()
       self.UploadTranslatedCaptions()
     except Error, e:
-      print "The captions were not successfully translated and submitted:"
+      print "The captions were not successfully translated and submitted."
       print e
     else:
       print "The captions were successfully translated and submitted."
@@ -213,11 +226,10 @@ class CaptionsDemo(object):
 if __name__ == "__main__":
   opt_parser = optparse.OptionParser()
   opt_parser.add_option("--video-id", help="A YouTube video id in your account.")
-  opt_parser.add_option('--target-language', help="The language to translate into.")
   options, arguments = opt_parser.parse_args()
 
-  if options.video_id is not None and options.target_language is not None:
-    captions_demo = CaptionsDemo(video_id=options.video_id, target_language=options.target_language)
+  if options.video_id is not None:
+    captions_demo = CaptionsDemo(video_id=options.video_id)
     captions_demo.main()
   else:
     opt_parser.print_help()
