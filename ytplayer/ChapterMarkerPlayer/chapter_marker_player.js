82ce1c777c2001c5ae474d746237e6b7702e7470
api.jeffy@gmail.com <api.jeffy@gmail.com@14958b2e-7955-0410-8a8d-a5d105d17ecf>
2012-06-18 21:05:51 +0000

    Updates to the JS code to clarify and inform!

--- ytplayer/ChapterMarkerPlayer/chapter_marker_player.js
+++ ytplayer/ChapterMarkerPlayer/chapter_marker_player.js
@@ -12,7 +12,7 @@
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and
  limitations under the License.
-*/
+ */
 
 // BEGIN_INCLUDE(namespace)
 window.ChapterMarkerPlayer = {
@@ -20,7 +20,18 @@ window.ChapterMarkerPlayer = {
   // existing HTML element on a page.
   insert: function(params) {
 // END_INCLUDE(namespace)
+    // We need to reserve 30px for the player's control bar when automatically sizing the player.
+    var YOUTUBE_CONTROLS_HEIGHT = 30;
+    // Assume a 9:16 (width:height) ratio when we need to calculate a player's height.
+    var PLAYER_HEIGHT_TO_WIDTH_RATIO = 9 / 16;
+    var DEFAULT_PLAYER_WIDTH = 400;
 // BEGIN_INCLUDE(validation1)
+    // params contains the following required and optional parameter names and values:
+    //   videoId: (required) The YouTube video id of the video to be embedded.
+    //   chapters: (required) Mapping of times (seconds since the video's start) to chapter titles.
+    //   width: (optional) The width of the embedded player. 400px is used by default.
+    //   playerOptions: (optional) An object corresponding to the options that can be passed to the
+    //                  YT.Player constructor. See https://developers.google.com/youtube/iframe_api_reference#Loading_a_Video_Player
     if (!('videoId' in params)) {
       throw 'The "videoId" parameter must be set to the YouTube video id to be embedded.';
     }
@@ -33,17 +44,13 @@ window.ChapterMarkerPlayer = {
     var times = [];
     for (var time in params.chapters) {
       if (params.chapters.hasOwnProperty(time)) {
-        times.push(time);
+        times.push(parseInt(time));
       }
     }
-    // The times are treated as strings, but we want them sorted numerically.
-    // This custom sort function will take care of that.
-    times.sort(function(a, b) {
-      return a - b;
-    });
+    // Sort the times numerically for display purposes.
+    times.sort();
 // END_INCLUDE(time_sort)
-    // Default to a 400px wide player and chapter marker section.
-    var width = params.width || 400;
+    var width = params.width || DEFAULT_PLAYER_WIDTH;
 
     if ('YT' in window && 'Player' in window.YT) {
       // If the iframe player API is already available, proceed to loading the player using the API.
@@ -63,16 +70,18 @@ window.ChapterMarkerPlayer = {
         // We only want to do this once, so it's protected by the
         // !('onYouTubePlayerAPIReady' in window) check.
         var scriptTag = document.createElement('script');
+        // This scheme-relative URL will use HTTPS if the host page is accessed via HTTPS,
+        // and HTTP otherwise.
         scriptTag.src = '//www.youtube.com/player_api';
         var firstScriptTag = document.getElementsByTagName('script')[0];
         firstScriptTag.parentNode.insertBefore(scriptTag, firstScriptTag);
 // END_INCLUDE(load_api)
       }
 // BEGIN_INCLUDE(queue_callbacks)
-      // Gracefully handle the situation where multiple ChapterMarkerPlayer.insert() calls are made
-      // before the YT.Player API is loaded by queueing up the functions which insert the player
-      // and chapter markers on the page. They'll be executed when onYouTubePlayerAPIReady() is
-      // invoked by the YT.Player API.
+      // We need to handle the situation where multiple ChapterMarkerPlayer.insert() calls are made
+      // before the YT.Player API is loaded. We do this by maintaining an array of functions, each
+      // of which adds a specific player and chapters. The functions will be executed when
+      // onYouTubePlayerAPIReady() is invoked by the YT.Player API.
       window.ChapterMarkerPlayer.onYouTubePlayerAPIReadyCallbacks.push(function() {
         insertPlayerAndAddChapterMarkers(params);
       });
@@ -83,7 +92,7 @@ window.ChapterMarkerPlayer = {
     // Calls the YT.Player constructor with the appropriate options to add the iframe player
     // instance to a parent element.
     // This is a private method that isn't exposed via the ChapterMarkerPlayer namespace.
-    var initializePlayer = function(containerElement, params) {
+    function initializePlayer(containerElement, params) {
       var playerContainer = document.createElement('div');
       containerElement.appendChild(playerContainer);
 
@@ -93,8 +102,11 @@ window.ChapterMarkerPlayer = {
       return new YT.Player(playerContainer, {
         // Maintain a 16:9 aspect ratio for the player based on the width passed in via params.
         // Override can be done via params.playerOptions if needed
-        height: playerOptions.height || width * 9 / 16 + 30,
+        height: playerOptions.height ||
+            width * PLAYER_HEIGHT_TO_WIDTH_RATIO + YOUTUBE_CONTROLS_HEIGHT,
         width: playerOptions.width || width,
+        // Unless playerVars are explicitly provided, use a reasonable default of { autohide: 1 },
+        // which hides the controls when the mouse isn't over the player.
         playerVars: playerOptions.playerVars || { autohide: 1 },
         videoId: params.videoId,
         events: {
@@ -105,50 +117,12 @@ window.ChapterMarkerPlayer = {
         }
       });
     }
+
 // END_INCLUDE(load_player)
 
-// BEGIN_INCLUDE(add_chapter_markers)
-    // Adds a sorted list of chapters below the player. Each chapter has a onclick handler that
-    // calls the iframe player API to seek to a specific timestamp in the video.
-    // This is a private method that isn't exposed via the ChapterMarkerPlayer namespace.
-    var addChapterMarkers = function(containerElement, player) {
-      var listContainerElement = document.createElement('ol');
-      listContainerElement.setAttribute('class', 'chapter-list');
-      listContainerElement.setAttribute('style', 'width: ' + width + 'px');
-      containerElement.appendChild(listContainerElement);
-
-      for (var i = 0; i < times.length; i++) {
-        var time = times[i];
-        var chapterTitle = params.chapters[time];
-
-        var liElement = document.createElement('li');
-        liElement.setAttribute('data-time', time);
-        liElement.textContent = formatTimestamp(time) + ': ' + chapterTitle;
-        liElement.onclick = function() {
-          // 'this' will refer to the element that was clicked.
-          player.seekTo(this.getAttribute('data-time'));
-        };
-        listContainerElement.appendChild(liElement);
-      }
-    }
-// END_INCLUDE(add_chapter_markers)
-
-    // Convenience method to call both initializePlayer and addChapterMarkers.
-    // This is a private method that isn't exposed via the ChapterMarkerPlayer namespace.
-    var insertPlayerAndAddChapterMarkers = function(params) {
-// BEGIN_INCLUDE(validation2)
-      var containerElement = document.getElementById(params.container);
-      if (!containerElement) {
-        throw 'The "container" parameter must be set to the id of a existing HTML element.';
-      }
-// END_INCLUDE(validation2)
-
-      var player = initializePlayer(containerElement, params);
-      addChapterMarkers(containerElement, player);
-    };
-
 // BEGIN_INCLUDE(format_timestamp)
-    var formatTimestamp = function(timestamp) {
+    // Takes a number of seconds and returns a #h##m##s string.
+    function formatTimestamp(timestamp) {
       var hours = Math.floor(timestamp / 3600);
       var minutes = Math.floor((timestamp - (hours * 3600)) / 60);
       var seconds = timestamp % 60;
@@ -162,8 +136,50 @@ window.ChapterMarkerPlayer = {
       }
 
       return formattedTimestamp;
-    };
+    }
+
 // END_INCLUDE(format_timestamp)
+
+// BEGIN_INCLUDE(add_chapter_markers)
+    // Adds a sorted list of chapters below the player. Each chapter has an onclick handler that
+    // calls the iframe player API to seek to a specific timestamp in the video.
+    // This is a private method that isn't exposed via the ChapterMarkerPlayer namespace.
+    function addChapterMarkers(containerElement, player) {
+      var ol = document.createElement('ol');
+      ol.setAttribute('class', 'chapter-list');
+      ol.setAttribute('style', 'width: ' + width + 'px');
+      containerElement.appendChild(ol);
+
+      for (var i = 0; i < times.length; i++) {
+        var time = times[i];
+        var chapterTitle = params.chapters[time];
+
+        var li = document.createElement('li');
+        li.setAttribute('data-time', time);
+        li.textContent = formatTimestamp(time) + ': ' + chapterTitle;
+        li.onclick = function() {
+          // 'this' will refer to the element that was clicked.
+          player.seekTo(this.getAttribute('data-time'));
+        };
+        ol.appendChild(li);
+      }
+    }
+
+// END_INCLUDE(add_chapter_markers)
+
+    // Convenience method to call both initializePlayer and addChapterMarkers.
+    // This is a private method that isn't exposed via the ChapterMarkerPlayer namespace.
+    function insertPlayerAndAddChapterMarkers(params) {
+// BEGIN_INCLUDE(validation2)
+      var containerElement = document.getElementById(params.container);
+      if (!containerElement) {
+        throw 'The "container" parameter must be set to the id of a existing HTML element.';
+      }
+// END_INCLUDE(validation2)
+
+      var player = initializePlayer(containerElement, params);
+      addChapterMarkers(containerElement, player);
+    }
   },
 // BEGIN_INCLUDE(callback_array)
   // This is used to keep track of the callback functions that need to be invoked when the iframe
