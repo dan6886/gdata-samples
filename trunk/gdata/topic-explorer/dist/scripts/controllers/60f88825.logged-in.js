15b269b93e561d980783841e2574e6fa48cc8fd0
api.jeffy@gmail.com <api.jeffy@gmail.com@14958b2e-7955-0410-8a8d-a5d105d17ecf>
2012-10-17 21:42:23 +0000

    Re-checking in topic-explorer, hopefully with proper mime types.

---
+++ trunk/gdata/topic-explorer/dist/scripts/controllers/60f88825.logged-in.js
@@ -0,0 +1,69 @@
+/*
+  Copyright 2012 Google Inc. All Rights Reserved.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+'use strict';
+
+topicExplorerApp.controller('LoggedInCtrl', ['$scope', '$rootScope', 'constants', 'youtube', function($scope, $rootScope, constants, youtube) {
+  $scope.thumbnailUrl = constants.DEFAULT_PROFILE_THUMBNAIL;
+
+  youtube({
+    method: 'GET',
+    service: 'channels',
+    params: {
+      mine: '',
+      part: 'id,snippet'
+    },
+    callback: function(response) {
+      $scope.$apply(function() {
+        $scope.title = (response.channels[0].snippet.title.split(/\W/))[0];
+        $scope.thumbnailUrl = response.channels[0].snippet.thumbnails.default.url;
+        $rootScope.channelId = response.channels[0].id;
+      });
+    }
+  });
+
+  // Ideally, this will load your subscriptions, then look up each channel and get any associated
+  // topics. Unfortunately, this doesn't work due to b/7365866
+  /*youtube({
+    method: 'GET',
+    service: 'subscriptions',
+    params: {
+      mine: '',
+      part: 'snippet'
+    },
+    cacheTimeoutMinutes: 0,
+    callback: function(subscriptionsResponse) {
+      var mids = {};
+
+      var channelIds = subscriptionsResponse.subscriptions.map(function(subscription) {
+        return subscription.snippet.resourceId.channelId;
+      });
+
+      youtube({
+        method: 'GET',
+        service: 'channels',
+        params: {
+          part: 'contentDetails,topicDetails',
+          id: channelIds.join(','),
+          maxResults: constants.YOUTUBE_API_MAX_RESULTS
+        },
+        callback: function(channelsResponse) {
+          console.log(channelsResponse);
+        }
+      });
+    }
+  });*/
+}]);
